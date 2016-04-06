a259af6f13689ef7efbe140d1d9675f471943c6a
Wojciech Hałapup <wojciech.halapup@hotmail.com>
2016-04-06 19:01:33 +0200

    kom

--- src/Zadanie_2/Main.java
+++ src/Zadanie_2/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
         Muzeum mojeMuzeum = new Muzeum(50);
-        new Thread(new BramaWejsciowa(mojeMuzeum, "Brama lewa")).start();
-        new Thread(new BramaWejsciowa(mojeMuzeum, "Brama prawa")).start();
-        new Thread(new BramaWyjsciowa(mojeMuzeum)).start();
+        //new Thread(new BramaWejsciowa(mojeMuzeum, "Brama lewa")).start();
+        //new Thread(new BramaWejsciowa(mojeMuzeum, "Brama prawa")).start();
+        //new Thread(new BramaWyjsciowa(mojeMuzeum)).start();
 	}
 }
